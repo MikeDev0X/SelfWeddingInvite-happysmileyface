b48b72d2cd1ba0d2a0d7ca4262b67f2d6219271a
Miguel Jimenez Padilla <miguel.jimenez@netlogistik.com>
2025-08-24 17:01:58 -0600

    implemented dynamic base for localhost

--- vite.config.ts
+++ vite.config.ts
@@ -2,11 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [react()],
   assetsInclude: ['**/*.mp3'],
-  base: '/SelfWeddingInvite-happysmileyface/',
+  base: command === 'serve' ? '/' : '/SelfWeddingInvite-happysmileyface/',
   build: {
-    outDir:'dist',
+    outDir: 'dist',
   },
-})
+}));
