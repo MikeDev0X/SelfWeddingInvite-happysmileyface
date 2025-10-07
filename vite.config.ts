8a75a1a7a80ec9676286003c081a3517a718ff9b
Miguel Jimenez Padilla <miguel.jimenez@netlogistik.com>
2025-10-06 23:01:32 -0600

    base config updated

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig(({ command }) => ({
   plugins: [react()],
   assetsInclude: ['**/*.mp3'],
-  base: command === 'serve' ? '/' : '/SelfWeddingInvite-happysmileyface/',
+  base: command === 'serve' ? '/' : '/',
   build: {
     outDir: 'dist',
   },
